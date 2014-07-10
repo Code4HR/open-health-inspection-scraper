5094be4ce46c2a7ee1ec35553277e371fb7954c6
kalmas <kylealmas@gmail.com>
2014-07-09 20:19:32 -0400

    make a mongo-persisted list of establishments to fetch

--- scraper/scrapeHealthData.py
+++ scraper/scrapeHealthData.py
@@ -8,51 +8,70 @@ print 'Connect to DB'
 db = mongolab.connect()
 va_establishments = db[c['state_abb']]
 
-print 'Find Cities'
-cities = scraper.get_cities()
+# collection of establishments to fetch
+to_fetch = db['establishments_to_fetch']
 
-for city in cities:
+# print 'Drop it like... well, you know'
+# to_fetch.drop()
 
-    print '\t' + city['name']
-    
-    establishments = scraper.get_establishments(city)
-    added = updated = 0
+if to_fetch.find_one() is None:
+    print 'Make a list of Establishments to Fetch'
+    print 'Finding Cities'
+    cities = scraper.get_cities()
 
-    for establishment in establishments:
+    for city in cities:
+        print 'Finding Establishments in ' + city['name']
 
-        existing = va_establishments.find_one({'url': establishment['url']})
+        establishments = scraper.get_establishments(city)
+        print 'Found ' + str(len(establishments)) + ' in ' + city['name']
 
-        if existing is not None:
-            if 'last_inspected_date' not in existing or \
-                            existing['last_inspected_date'] < establishment['last_inspected_date']:
-                changed_fields = []
-                establishment = scraper.get_establishment_details(establishment)
-                establishment['inspections'] = scraper.get_inspections(establishment, city['baseUrl'])
-                establishment['_id'] = existing['_id']
-                changed_fields = list(o for o in establishment if existing[o] != establishment[o])
-            else:
-                continue
-        else:
+        for establishment in establishments:
+            to_fetch.insert(establishment)
+
+
+print 'Start Fetching Establishment Data'
+establishments = to_fetch.find()
+added = updated = 0
+
+for establishment in establishments:
+
+    existing = va_establishments.find_one({'url': establishment['url']})
+
+    if existing is not None:
+        if 'last_inspected_date' not in existing or \
+                existing['last_inspected_date'] < establishment['last_inspected_date']:
+            changed_fields = []
             establishment = scraper.get_establishment_details(establishment)
-            establishment['inspections'] = scraper.get_inspections(establishment, city['baseUrl'])
-            establishment['_id'] = None
-            changed_fields = ['None']
+            establishment['inspections'] = scraper.get_inspections(establishment, establishment['baseUrl'])
+            # establishment['_id'] = existing['_id']
+            changed_fields = list(o for o in establishment if existing[o] != establishment[o])
+        else:
+            continue
+    else:
+        establishment = scraper.get_establishment_details(establishment)
+        establishment['inspections'] = scraper.get_inspections(establishment, establishment['baseUrl'])
+        # establishment['_id'] = None
+        changed_fields = ['None']
 
-        if changed_fields:
-            if any(key in changed_fields for key in ('address', 'city', 'None')):
-                establishment = scraper.get_establishment_geo(establishment)
+    if changed_fields:
+        if any(key in changed_fields for key in ('address', 'city', 'None')):
+            pass
+            # establishment = scraper.get_establishment_geo(establishment)
 
-            va_establishments.update({'_id': establishment['_id']},
-                                     establishment,
-                                     True)
+        va_establishments.update({'_id': establishment['_id']},
+                                 establishment,
+                                 True)
 
-            if 'None' in changed_fields:
-                print '\t' + establishment['name'] + ' Added!'
-                added += 1
-            else:
-                print '\t' + establishment['name'] + ' Updated!'
-                updated += 1
+        if 'None' in changed_fields:
+            print '\t' + establishment['name'] + ' Added!'
+            added += 1
+        else:
+            print '\t' + establishment['name'] + ' Updated!'
+            updated += 1
 
-    print str(added) + ' new vendors added'
-    print str(updated) + ' existing vendors updated'
+    to_fetch.remove({'_id': establishment['_id']})
+
+
+print str(added) + ' new vendors added'
+print str(updated) + ' existing vendors updated'
 
