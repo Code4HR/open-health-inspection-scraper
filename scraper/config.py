3a833c3307c94314a2c5c1d9dc1966709d61cfcc
Sean Brewer <seabre986@gmail.com>
2014-08-27 21:46:21 -0400

    Allow use of environment variables instead of using config.json

--- scraper/config.py
+++ scraper/config.py
@@ -1,6 +1,38 @@
 import json
+import os
+
+ENV_DB_URI = 'SCRAPER_DB_URI'
+ENV_DB_NAME = 'SCRAPER_DB_NAME'
+ENV_SS_ID = 'SCRAPER_SS_ID'
+ENV_SS_TOKEN = 'SCRAPER_SS_TOKEN'
+ENV_STATE = 'SCRAPER_STATE'
+ENV_STATE_ABB = 'SCRAPER_STATE_ABB'
+ENV_VARIABLES = [
+  ENV_DB_URI,
+  ENV_DB_NAME,
+  ENV_SS_ID,
+  ENV_SS_TOKEN,
+  ENV_STATE,
+  ENV_STATE_ABB
+]
+
+def uses_env_vars():
+    return reduce(lambda i,j: i and j,
+        map(lambda k: os.environ.get(k) != None, ENV_VARIABLES))
+
+def environ_config():
+    return {
+        'db_uri': os.environ[ENV_DB_URI],
+        'db_name': os.environ[ENV_DB_NAME],
+        'ss_id': os.environ[ENV_SS_ID],
+        'ss_token': os.environ[ENV_SS_TOKEN],
+        'state': os.environ[ENV_STATE],
+        'state_abb': os.environ[ENV_STATE_ABB]
+    }
 
 def load():
-    with open('config.json','r') as f:
-        return json.loads(f.read())
-
+    if uses_env_vars():
+        return environ_config()
+    else:
+        with open('config.json','r') as f:
+            return json.loads(f.read())
